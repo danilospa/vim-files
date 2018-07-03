3189fc536adeed3b8a556d0d400c17cb44c63164
Danilo Albuquerque <danilospalbuquerque@gmail.com>
2018-07-03 16:34:27 -0300

    Add bash_profile extension
    This was added as an extension to the existing bash_profile due to
    possible sensitive information present on it. Other reason is the
    existing of initialization commands for specific applications of the
    machine, which can not be overwrited.

--- setup.sh
+++ setup.sh
@@ -8,3 +8,8 @@ ln -s $(pwd)/.inputrc ~/.inputrc
 pushd .vim/bundle/vimproc.vim
 make
 popd
+
+EXTENSION_INCLUDED=$(cat ~/.bash_profile | grep $(pwd)/.bash_profile_extension)
+if [ -z "$EXTENSION_INCLUDED" ]; then
+  echo source $(pwd)/.bash_profile_extension >> ~/.bash_profile
+fi
