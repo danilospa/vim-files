47c355103dcd4367cfdbeb609b6c5cf5ac3bcbe5
Danilo Albuquerque <danilospalbuquerque@gmail.com>
2020-04-10 15:18:30 -0300

    Clone tmux plugin manager on setup

--- setup.sh
+++ setup.sh
@@ -10,6 +10,8 @@ ln -s $(pwd)/.inputrc ~/.inputrc
 
 git config --global core.excludesfile $(pwd)/.gitignore.global
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 EXTENSION_INCLUDED=$(cat ~/.bash_profile | grep $(pwd)/.bash_profile_extension)
 if [ -z "$EXTENSION_INCLUDED" ]; then
   echo source $(pwd)/.bash_profile_extension >> ~/.bash_profile
